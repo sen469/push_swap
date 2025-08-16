c7c87cdf869307ef895775d59675238e842a1592
sen46 <sw20050719@gmail.com>
2025-08-16 16:33:04 +0900

    first commit

---
+++ b_srcs/checker.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssawa <ssawa@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 22:16:04 by ssawa             #+#    #+#             */
+/*   Updated: 2025/08/16 16:22:14 by ssawa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+#include "push_swap.h"
+#include "struct.h"
+#include "libft/get_next_line.h"
+
+static void	init_stacks(t_node **st_a, t_node **st_b)
+{
+	*st_a = (t_node *)malloc(sizeof(t_node));
+	*st_b = (t_node *)malloc(sizeof(t_node));
+	if (!*st_a || !*st_b)
+		error_exit();
+	(*st_a)->prev = *st_a;
+	(*st_a)->next = *st_a;
+	(*st_b)->prev = *st_b;
+	(*st_b)->next = *st_b;
+}
+
+static void	gnl_and_do_command(t_node *st_a, t_node *st_b)
+{
+	char	*line;
+
+	line = get_next_line(0);
+	while (line)
+	{
+		do_command_checker(st_a, st_b, line);
+		free(line);
+		line = get_next_line(0);
+	}
+	get_next_line(-1);
+}
+
+int	is_sorted_and_b_empty(t_node *st_a, t_node *st_b)
+{
+	t_node	*tmp;
+
+	if (get_stack_size(st_b) != 0)
+		return (0);
+	if (get_stack_size(st_a) <= 1)
+		return (1);
+	tmp = st_a->next;
+	while (tmp->next != st_a)
+	{
+		if (tmp->data > tmp->next->data)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
+static void	print_result(t_node *st_a, t_node *st_b)
+{
+	if (is_sorted_and_b_empty(st_a, st_b))
+		write(1, "OK\n", 3);
+	else
+		write(1, "KO\n", 3);
+}
+
+int	main(int argc, char **argv)
+{
+	t_node	*st_a;
+	t_node	*st_b;
+	t_vec	vec;
+
+	if (argc == 1)
+		return (0);
+	if (valid(argc, argv, &vec))
+		error_exit();
+	init_stacks(&st_a, &st_b);
+	init(&vec, st_a);
+	free(vec.arr);
+	gnl_and_do_command(st_a, st_b);
+	print_result(st_a, st_b);
+	free_stack(&st_a);
+	free_stack(&st_b);
+	return (0);
+}
---
+++ b_srcs/judge.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   judge.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssawa <ssawa@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 22:17:10 by ssawa             #+#    #+#             */
+/*   Updated: 2025/08/16 16:12:04 by ssawa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+#include "libft/libft.h"
+#include "push_swap.h"
+
+void	do_command_checker(t_node *st_a, t_node *st_b, char *cmd)
+{
+	if (!ft_strcmp(cmd, RA))
+		do_ra(st_a, 0);
+	else if (!ft_strcmp(cmd, RB))
+		do_rb(st_b, 0);
+	else if (!ft_strcmp(cmd, RR))
+		do_rr(st_a, st_b, 0);
+	else if (!ft_strcmp(cmd, RRA))
+		do_rra(st_a, 0);
+	else if (!ft_strcmp(cmd, RRB))
+		do_rrb(st_b, 0);
+	else if (!ft_strcmp(cmd, RRR))
+		do_rrr(st_a, st_b, 0);
+	else if (!ft_strcmp(cmd, SA))
+		do_sa(st_a, 0);
+	else if (!ft_strcmp(cmd, SB))
+		do_sb(st_b, 0);
+	else if (!ft_strcmp(cmd, SS))
+		do_ss(st_a, st_b, 0);
+	else if (!ft_strcmp(cmd, PA))
+		do_pa(st_a, st_b, 0);
+	else if (!ft_strcmp(cmd, PB))
+		do_pb(st_a, st_b, 0);
+}
